dc5466bb0373767666b100cea0f165960f46f5d9
Sladamos <sladamo@wp.pl>
2023-05-29 00:16:20 +0200

    Integration test for Iron production

---
+++ src/test/java/integration/FromIronOreToProductsTest.java
@@ -0,0 +1,31 @@
+package integration;
+
+import org.junit.jupiter.api.Test;
+import strategy.kingdom.building.producer.bar.IronBarSmelter;
+import strategy.kingdom.building.producer.mine.basic.IronMiner;
+import strategy.kingdom.building.producer.tool.bucket.IronBucketArtisan;
+import strategy.kingdom.building.producer.weapon.meele.IronSwordBlacksmith;
+import strategy.kingdom.product.tool.bucket.IronBucket;
+import strategy.kingdom.product.weapon.meele.sword.IronSword;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FromIronOreToProductsTest {
+    @Test
+    public void Should_ProduceBucketAndSword_When_IronBarsInStorage() {
+        IronMiner miner = new IronMiner(0);
+        IronBarSmelter smelter = new IronBarSmelter(miner::getOre, 0);
+        IronBucketArtisan artisan = new IronBucketArtisan(smelter::getBar,0);
+        IronSwordBlacksmith blacksmith = new IronSwordBlacksmith(smelter::getBar, 0);
+        Thread thread = new Thread(artisan);
+        thread.start();
+        thread = new Thread(blacksmith);
+        thread.start();
+        thread = new Thread(smelter);
+        thread.start();
+        thread = new Thread(miner);
+        thread.start();
+        assertThat(artisan.getTool()).isInstanceOf(IronBucket.class);
+        assertThat(blacksmith.getWeapon()).isInstanceOf(IronSword.class);
+    }
+}
