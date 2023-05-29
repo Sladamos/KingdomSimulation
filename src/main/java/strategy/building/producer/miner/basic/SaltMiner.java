e1ed40bc76701ebf455e6e87918a5291f00a60c9
Sladamos <sladamo@wp.pl>
2023-05-29 11:52:50 +0200

    New mines

---
+++ src/main/java/strategy/building/producer/miner/basic/SaltMiner.java
@@ -0,0 +1,19 @@
+package strategy.building.producer.miner.basic;
+
+import strategy.material.mineral.Salt;
+
+public class SaltMiner extends Miner<Salt> {
+
+	private static final int SALT_MINER_DURABILITY = 200;
+
+	private static final int SALT_MINER_MINING_SPEED = 4;
+
+	public SaltMiner(int defaultStorageSize) {
+		super(defaultStorageSize, SALT_MINER_MINING_SPEED, SALT_MINER_DURABILITY);
+	}
+
+	@Override
+	protected Salt createNewOre() {
+		return new Salt();
+	}
+}
