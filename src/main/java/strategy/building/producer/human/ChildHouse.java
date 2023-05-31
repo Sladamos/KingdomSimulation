b19eb88ec660d59d05f4624252a0fca69e9edd8b
Sladamos <sladamo@wp.pl>
2023-05-31 16:05:26 +0200

    House logic modified

--- src/main/java/strategy/building/producer/human/ChildHouse.java
+++ src/main/java/strategy/building/producer/human/ChildHouse.java
@@ -1,24 +1,24 @@
-package strategy.building.producer.child;
+package strategy.building.producer.human;
 
-import strategy.building.producer.OneToOneProducer;
 import strategy.organism.human.Child;
 import strategy.product.coin.Coin;
+import strategy.product.food.baking.bread.Bread;
 
 import java.util.function.Supplier;
 
-public class ChildHouse<T extends Coin> extends House<T, Child> {
+public class ChildHouse<T extends Coin, U extends Bread> extends House<T, U, Child> {
 
 	private static final int CHILD_HOUSE_DURABILITY = 100;
 
 	private static final int CHILD_HOUSE_PRODUCING_SPEED = 1;
 
-	public ChildHouse(Supplier<T> producer, int defaultStorageSize) {
-		super(producer, defaultStorageSize, CHILD_HOUSE_PRODUCING_SPEED,
+	public ChildHouse(Supplier<T> firstProducer, Supplier<U> secondProducer, int defaultStorageSize) {
+		super(firstProducer, secondProducer, defaultStorageSize, CHILD_HOUSE_PRODUCING_SPEED,
 				CHILD_HOUSE_DURABILITY);
 	}
 
 	@Override
-	protected Child produceNewItem(T material) {
+	protected Child produceNewItem(T material, U secondMaterial) {
 		return new Child(0, 100);
 	}
 
---
+++ src/main/java/strategy/building/producer/human/House.java
@@ -0,0 +1,15 @@
+package strategy.building.producer.human;
+
+import strategy.building.producer.TwoToOneProducer;
+import strategy.organism.human.Human;
+import strategy.product.coin.Coin;
+import strategy.product.food.Food;
+
+import java.util.function.Supplier;
+
+public abstract class House<T extends Coin, U extends Food, V extends Human> extends TwoToOneProducer<T, U, V> {
+
+	public House(Supplier<T> firstProducer, Supplier<U> secondProducer, int defaultStorageSize, double producingSpeed, int durability) {
+		super(firstProducer, secondProducer, defaultStorageSize, producingSpeed, durability);
+	}
+}
