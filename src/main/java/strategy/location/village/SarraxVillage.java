edd2cdd003e41a5ce6f9c1f29a11936f9957743d
Sladamos <sladamo@wp.pl>
2023-05-31 16:13:01 +0200

    Wood producer in village

--- src/main/java/strategy/location/village/SarraxVillage.java
+++ src/main/java/strategy/location/village/SarraxVillage.java
@@ -1,27 +1,35 @@
 package strategy.location.village;
 
+import strategy.building.producer.farm.Farm;
 import strategy.building.producer.farm.WheatFarm;
 import strategy.building.producer.livestock.Apiary;
 import strategy.building.producer.livestock.Cow;
+import strategy.building.producer.livestock.LivestockAnimal;
+import strategy.building.producer.lumberjack.Lumberjack;
+import strategy.building.producer.lumberjack.MahoganyLumberjack;
 import strategy.material.food.Honey;
 import strategy.material.food.Milk;
 import strategy.material.plant.Wheat;
+import strategy.material.wood.Mahogany;
 import strategy.product.fluid.Water;
 
 import java.util.function.Supplier;
 
 public class SarraxVillage implements Village, Runnable {
 
-	private final WheatFarm farm;
+	private final Farm<Water, Wheat> farm;
 
-	private final Apiary apiary;
+	private final LivestockAnimal<Honey> apiary;
 
-	private final Cow cow;
+	private final LivestockAnimal<Milk> cow;
+
+	private final Lumberjack<Mahogany> lumberjack;
 
 	public SarraxVillage(Supplier<Water> waterSupplier) {
 		cow = new Cow(4);
 		apiary = new Apiary(2);
 		farm = new WheatFarm(waterSupplier, 12);
+		lumberjack = new MahoganyLumberjack(14);
 	}
 
 	public synchronized Wheat getWheat() {
@@ -36,6 +44,10 @@ public class SarraxVillage implements Village, Runnable {
 		return apiary.getFood();
 	}
 
+	public synchronized Mahogany getWood() {
+		return lumberjack.getWood();
+	}
+
 	@Override
 	public void run() {
 		Thread thread = new Thread(apiary);
@@ -43,6 +55,8 @@ public class SarraxVillage implements Village, Runnable {
 		thread = new Thread(cow);
 		thread.start();
 		thread = new Thread(farm);
+		thread.start();
+		thread = new Thread(lumberjack);
 		thread.start();
 	}
 }
