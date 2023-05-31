d24419c96fabc7d19a7f266722c7383e352a0153
Sladamos <sladamo@wp.pl>
2023-05-31 22:33:36 +0200

    Royal preparation

---
+++ src/main/java/strategy/producer/royal/queen/Queen.java
@@ -0,0 +1,6 @@
+package strategy.producer.royal.queen;
+
+import strategy.producer.royal.Royal;
+
+public interface Queen extends Royal {
+}
---
+++ src/main/java/strategy/producer/royal/queen/SarraxQueen.java
@@ -0,0 +1,35 @@
+package strategy.producer.royal.queen;
+
+import strategy.organism.human.Adult;
+import strategy.organism.human.Child;
+import strategy.producer.TwoToOneProducer;
+import strategy.product.elixir.GrowthElixir;
+
+import java.util.function.Supplier;
+
+public class SarraxQueen extends TwoToOneProducer<Child, GrowthElixir, Adult> implements Queen {
+
+	private static final int QUEEN_TRANSFORMATING_SPEED = 1;
+
+	private static final int QUEEN_DURABILITY = 1000;
+
+	public SarraxQueen(Supplier<Child> firstProducer, Supplier<GrowthElixir> secondProducer) {
+		super(firstProducer, secondProducer, 0, QUEEN_TRANSFORMATING_SPEED,
+				QUEEN_DURABILITY);
+	}
+
+	@Override
+	protected Adult produceNewItem(Child material, GrowthElixir secondMaterial) {
+		return new Adult(10, 100);
+	}
+
+	@Override
+	protected Adult produceNewItem() {
+		return new Adult(10, 100);
+	}
+
+	@Override
+	protected int getProducingTime() {
+		return 3000;
+	}
+}
