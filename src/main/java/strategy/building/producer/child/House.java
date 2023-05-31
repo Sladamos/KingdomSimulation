89900b55d007e239f284535b471d20ccbabc7619
Sladamos <sladamo@wp.pl>
2023-05-31 15:22:50 +0200

    House created

---
+++ src/main/java/strategy/building/producer/child/House.java
@@ -0,0 +1,19 @@
+package strategy.building.producer.child;
+
+import strategy.building.producer.OneToOneProducer;
+import strategy.organism.human.Human;
+import strategy.product.coin.Coin;
+
+import java.util.function.Supplier;
+
+public abstract class House<T extends Coin, U extends Human> extends OneToOneProducer<T, U> {
+
+	public House(Supplier<T> producer, int defaultStorageSize, double producingSpeed, int durability) {
+		super(producer, defaultStorageSize, producingSpeed, durability);
+	}
+
+	@Override
+	protected int getProducingTime() {
+		return 50000;
+	}
+}
---
+++ src/main/java/strategy/organism/human/Child.java
@@ -0,0 +1,8 @@
+package strategy.organism.human;
+
+public class Child extends Human {
+
+	public Child(int initialHunger, int hungerLimit) {
+		super(initialHunger, hungerLimit);
+	}
+}
