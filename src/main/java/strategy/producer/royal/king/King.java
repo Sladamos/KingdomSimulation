0d29c3731983493f6da41cffcaf0a4ea301819f2
Sladamos <sladamo@wp.pl>
2023-06-02 00:21:43 +0200

    King refactored

--- src/main/java/strategy/producer/royal/king/King.java
+++ src/main/java/strategy/producer/royal/king/King.java
@@ -1,23 +1,6 @@
 package strategy.producer.royal.king;
 
-import strategy.producer.TwoToTwoProducer;
-import strategy.producer.building.craftsman.present.PresentCraftsman;
-import strategy.producer.building.jewellery.basic.Jeweller;
-import strategy.producer.exceptions.ProducerDestroyedException;
-import strategy.product.present.Present;
+import strategy.producer.royal.Royal;
 
-
-public abstract class King <T extends PresentCraftsman<?, U>, S extends PresentCraftsman<?, V>, U extends Present, V extends Present> extends TwoToTwoProducer<T, S, U, V> {
-
-	public King(T firstProducer, S secondProducer) {
-		super(firstProducer, secondProducer);
-	}
-
-	public synchronized U getFirstPresent() {
-		return getFirstItem();
-	}
-
-	public synchronized V getSecondPresent() {
-		return getSecondItem();
-	}
+public interface King extends Royal {
 }
--- src/main/java/strategy/producer/royal/king/SarraxKing.java
+++ src/main/java/strategy/producer/royal/king/SarraxKing.java
@@ -1,42 +1,17 @@
 package strategy.producer.royal.king;
 
+import strategy.producer.TwoToTwoProducer;
 import strategy.producer.building.craftsman.present.PresentCraftsman;
 import strategy.product.jewellery.necklace.RubyNecklace;
 import strategy.product.jewellery.ring.SapphireRing;
 import strategy.product.present.NecklacePresent;
 import strategy.product.present.RingPresent;
 
-public class SarraxKing extends King<PresentCraftsman<RubyNecklace, NecklacePresent>,
-		PresentCraftsman<SapphireRing, RingPresent>, NecklacePresent, RingPresent> {
+public class SarraxKing extends TwoToTwoProducer<PresentCraftsman<RubyNecklace, NecklacePresent>,
+		PresentCraftsman<SapphireRing, RingPresent>, NecklacePresent, RingPresent> implements King {
 
 	public SarraxKing(PresentCraftsman<RubyNecklace, NecklacePresent> firstProducer,
 	                  PresentCraftsman<SapphireRing, RingPresent> secondProducer) {
 		super(firstProducer, secondProducer);
-		/*
-		var firstCraftsman = new PresentCraftsman<RubyNecklace, NecklacePresent>(firstProducer::getItem, 0) {
-			@Override
-			protected NecklacePresent produceNewItem(RubyNecklace necklace) {
-				return new NecklacePresent();
-			}
-
-			@Override
-			protected NecklacePresent produceNewItem() {
-				return new NecklacePresent();
-			}
-		};
-		
-		var secondCraftsman =  new PresentCraftsman<SapphireRing, RingPresent>(secondProducer::getItem, 0) {
-			@Override
-			protected RingPresent produceNewItem(SapphireRing necklace) {
-				return new RingPresent();
-			}
-
-			@Override
-			protected RingPresent produceNewItem() {
-				return new RingPresent();
-			}
-		};
-
-		 */
 	}
 }
