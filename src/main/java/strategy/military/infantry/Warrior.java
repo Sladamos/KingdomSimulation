22fd4c11fccb7fd1dba52295206a2b3e3ad46c6b
Sladamos <sladamo@wp.pl>
2023-06-03 13:28:06 +0200

    Warrior toString

--- src/main/java/strategy/military/infantry/Warrior.java
+++ src/main/java/strategy/military/infantry/Warrior.java
@@ -5,4 +5,9 @@ public class Warrior extends HumanInfantryUnit {
     public Warrior(int damage, int defense) {
         super(damage, defense);
     }
+
+    @Override
+    public String toString() {
+        return "Warrior";
+    }
 }
