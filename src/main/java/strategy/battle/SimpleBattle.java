77774a1f52e5ad6aa7e72d3b241603d901de075f
Sladamos <sladamo@wp.pl>
2023-06-03 12:38:14 +0200

    Simple battle modified and finished

--- src/main/java/strategy/battle/SimpleBattle.java
+++ src/main/java/strategy/battle/SimpleBattle.java
@@ -8,30 +8,52 @@ public class SimpleBattle implements Battle {
 
     private final Kingdom secondKingdom;
 
+    private boolean areFighting;
+
     public SimpleBattle(Kingdom firstKingdom, Kingdom secondKingdom) {
         this.firstKingdom = firstKingdom;
         this.secondKingdom = secondKingdom;
+        areFighting = false;
     }
 
     @Override
     public void run() {
-        while(true) {
+        areFighting = true;
+        while(areFighting) {
+            firstAttack();
+            if(areFighting) {
+                secondAttack();
+            }
+        }
+    }
+
+    private void secondAttack() {
+        try {
+            Thread.sleep(15000);
+            System.out.println("Second attacks");
+            secondKingdom.attack(firstKingdom);
+            if (!firstKingdom.canFight()) {
+                System.out.println("Second kingdom won the battle");
+                areFighting = false;
+            }
+        } catch (InterruptedException ignored) {
+            areFighting = false;
+        }
+    }
+
+    private void firstAttack() {
+        try {
+            Thread.sleep(15000);
+            System.out.println("First attacks");
             firstKingdom.attack(secondKingdom);
             if(!secondKingdom.canFight()) {
                 System.out.println("First kingdom won the battle");
-                break;
+                areFighting = false;
             }
-            try {
-                Thread.sleep(15000);
-            } catch (InterruptedException e) {
-                break;
-            }
-            secondKingdom.attack(firstKingdom);
-            if(!firstKingdom.canFight()) {
-                System.out.println("Second kingdom won the battle");
-                break;
-            }
+        } catch (InterruptedException ignored) {
+            areFighting = false;
         }
+
     }
 
 }
