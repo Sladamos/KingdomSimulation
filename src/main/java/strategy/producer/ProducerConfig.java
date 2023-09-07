21219caea3757d933f21850e1a1b6a7fe9ee9a61
Sladamos <sladamo@wp.pl>
2023-09-07 18:33:59 +0200

    Producer config

---
+++ src/main/java/strategy/producer/ProducerConfig.java
@@ -0,0 +1,10 @@
+package strategy.producer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ProducerConfig {
+    int numberOfItemsPerMinute;
+}
