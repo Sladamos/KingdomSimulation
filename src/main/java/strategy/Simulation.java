398f9c4978fa1928bdd2cd1e68b271ba479ee08f
Sladamos <sladamo@wp.pl>
2023-09-07 18:33:53 +0200

    New todos

--- src/main/java/strategy/Simulation.java
+++ src/main/java/strategy/Simulation.java
@@ -4,22 +4,23 @@ import strategy.initializer.SimulationInitializer;
 
 public class Simulation {
 
-    //TODO gui:
-    /*
-    two panels with names of materials and count of each one (consider it!):
-    three buttons -> Start developing / add infantry  -> new Window (number / maxDamage / maxDefense)/ terminate
-    one button -> start battle
-    mid panel -> battle results
-    button to clear panel
-    layer for error handling -> both for console and gui
+    //TODO: validate config file in config initializers (are values > 0 etc.)
+
+    /*TODO gui:
+        two panels with names of materials and count of each one (consider it!):
+        three buttons -> Start developing / add infantry  -> new Window (number / maxDamage / maxDefense)/ terminate
+        one button -> start battle
+        mid panel -> battle results
+        button to clear panel
+        layer for error handling -> both for console and gui
      */
 
     //TODO: before simulation initialize there should be created all gui (ApplicationMessenger)
-    //then there should exist: BattleReceiver FirstKingdomMessenger SecondKingdomMessenger : for example in
-    //battlereceiver create StringProperty, and change it when new messsage will come.
+    //  then there should exist: BattleReceiver FirstKingdomMessenger SecondKingdomMessenger : for example in
+    //  battlereceiver create StringProperty, and change it when new messsage will come.
 
     //TODO: error handling layer, when critical app error catched display error and shut down app , when app error display it only
-    //PROBLEM: how to shut down child threads?
+    //  Also handle exception: SafeDisable(); -> then display something and end app
     public static void main(String[] args) {
         SimulationInitializer simulationInitializer = new SimulationInitializer();
         simulationInitializer.initializeSimulation();
