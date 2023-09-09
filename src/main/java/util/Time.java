0aa9741306eaf98e72f07b0ea3546ac2611a55b8
Sladamos <sladamo@wp.pl>
2023-09-09 13:53:12 +0200

    Time implemented

---
+++ src/main/java/util/Time.java
@@ -0,0 +1,6 @@
+package util;
+
+public interface Time {
+    long getMiliseconds();
+    int getSeconds();
+}
---
+++ src/main/java/util/TimeImpl.java
@@ -0,0 +1,19 @@
+package util;
+
+public class TimeImpl implements Time {
+    private final int seconds;
+
+    public TimeImpl(int seconds) {
+        this.seconds = seconds;
+    }
+
+    @Override
+    public long getMiliseconds() {
+        return seconds * 1000L;
+    }
+
+    @Override
+    public int getSeconds() {
+        return seconds;
+    }
+}
