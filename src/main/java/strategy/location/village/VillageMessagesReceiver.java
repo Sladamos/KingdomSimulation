adda1614b06ccb187167f6d0d295bae11e3bb763
Sladamos <sladamo@wp.pl>
2023-09-11 00:19:22 +0200

    Locations messages receivers

--- src/main/java/strategy/location/village/VillageMessagesReceiver.java
+++ src/main/java/strategy/location/village/VillageMessagesReceiver.java
@@ -1,15 +1,22 @@
 package strategy.location.village;
 
 import strategy.events.oneargevent.OneArgEvent;
+import strategy.events.oneargevent.OneArgEventImpl;
 import strategy.message.JSONMessage;
+import strategy.message.MessagesNotifier;
 import strategy.message.receiver.MessagesReceiver;
 
 public class VillageMessagesReceiver implements MessagesReceiver<JSONMessage> {
 
-    private OneArgEvent<JSONMessage> messageEvent;
+    private final OneArgEvent<JSONMessage> messageEvent;
+
+    public VillageMessagesReceiver(MessagesNotifier<JSONMessage> messagesNotifier) {
+        messageEvent = new OneArgEventImpl<>();
+        messagesNotifier.addListener(this);
+    }
 
     @Override
-    public void receiveMessage(JSONMessage message) {
+    public void accept(JSONMessage message) {
         JSONMessage newMessage = new JSONMessage(message);
         newMessage.put("sender", "village");
         messageEvent.invoke(newMessage);
