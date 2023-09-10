711854b0ab840954b550a6c2bce32b9601feaaf8
Sladamos <sladamo@wp.pl>
2023-09-11 00:51:10 +0200

    Storage method updated

--- src/main/java/strategy/storage/OneItemStorage.java
+++ src/main/java/strategy/storage/OneItemStorage.java
@@ -4,11 +4,12 @@ import strategy.StorageManager;
 import strategy.item.Item;
 import strategy.message.JSONMessage;
 import strategy.message.Message;
+import strategy.message.receiver.MessagesReceiver;
 
 import java.util.function.Consumer;
 
 public interface OneItemStorage<T extends Item> extends StorageManager {
-    void addListener(Consumer<JSONMessage> messageConsumer);
+    void addListener(MessagesReceiver<JSONMessage> messageConsumer);
     void addItemToStorage(T item);
     T getItemFromStorage();
 }
