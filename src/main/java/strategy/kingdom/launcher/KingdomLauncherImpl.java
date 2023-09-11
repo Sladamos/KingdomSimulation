c5dda0346b20e4804e2136c82775c9454f6f48b2
Sladamos <sladamo@wp.pl>
2023-09-11 14:31:54 +0200

    Kingdom launcher which launches kingdoms for developing with specified time

---
+++ src/main/java/strategy/kingdom/launcher/KingdomLauncherImpl.java
@@ -0,0 +1,39 @@
+package strategy.kingdom.launcher;
+
+import strategy.kingdom.Kingdom;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class KingdomLauncherImpl implements KingdomLauncher {
+
+    private final ExecutorService executor;
+
+    public KingdomLauncherImpl() {
+        executor = Executors.newCachedThreadPool();
+    }
+
+    @Override
+    public void launchKingdom(Kingdom kingdom, long miliseconds) {
+        executor.submit(() -> simulateKingdomDevelop(kingdom, miliseconds));
+    }
+
+    @Override
+    public void waitForDevelopingEnd() {
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (Exception ignored) {
+        }
+    }
+
+    private void simulateKingdomDevelop(Kingdom kingdom, long sleep) {
+        kingdom.run();
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException ignored) {
+        }
+        kingdom.terminate();
+    }
+}
