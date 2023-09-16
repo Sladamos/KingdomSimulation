ad491033f08e5307b368c08b547bd77f6685548b
Sladamos <sladamo@wp.pl>
2023-09-16 22:19:19 +0200

    Template for advanced attack test

---
+++ src/test/java/attack/AdvancedAttackTest.java
@@ -0,0 +1,26 @@
+package attack;
+
+import org.junit.jupiter.api.Test;
+
+public class AdvancedAttackTest {
+
+	@Test
+	public void throwCriticalAppError_when_passedNegativeDamage() {
+
+	}
+
+	@Test
+	public void doesNotThrowAnyException_when_attackerIsNotPassed() {
+
+	}
+
+	@Test
+	public void doesNotThrowAnyException_when_damageIsEqualToZero() {
+
+	}
+
+	@Test
+	public void returnOptional_when_getFightableIsCalled() {
+
+	}
+}
