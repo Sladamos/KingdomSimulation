4bb311343c7935d528f9f18e294bb84d504b532d
Sladamos <sladamo@wp.pl>
2023-09-17 18:00:49 +0200

    Test update

--- src/test/java/parser/KingdomTypeParserTest.java
+++ src/test/java/parser/KingdomTypeParserTest.java
@@ -3,6 +3,7 @@ package parser;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import strategy.error.BasicAppError;
 import strategy.error.CriticalAppError;
 import strategy.kingdom.type.KingdomType;
 import strategy.kingdom.type.KingdomTypeParser;
@@ -19,11 +20,11 @@ public class KingdomTypeParserTest {
 
 	@Test
 	public void returnSarraxType_when_stringSarraxPassed() {
-		assertThat(parser.parse("sarrax")).isEqualTo(KingdomType.SARRAX);
+		assertThat(parser.parse("Sarrax")).isEqualTo(KingdomType.SARRAX);
 	}
 
 	@Test
-	public void throwCriticalAppException_when_incorrectStringPassed() {
-		assertThatThrownBy(() -> parser.parse("incorrect_type")).isInstanceOf(CriticalAppError.class);
+	public void throwBasicAppException_when_incorrectStringPassed() {
+		assertThatThrownBy(() -> parser.parse("incorrect_type")).isInstanceOf(BasicAppError.class);
 	}
 }
